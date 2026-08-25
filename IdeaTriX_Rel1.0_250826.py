b05b1b440a9bc26bda2c92605a9934ffa9634266
tusharkanikdale-source <tushar.kanikdale@gmail.com>
2026-08-25 21:35:32 +0530

    Update IdeaTriX_Rel1.0_250826.py

--- IdeaTriX_Rel1.0_250826.py
+++ IdeaTriX_Rel1.0_250826.py
@@ -6,7 +6,7 @@ import base64
 import streamlit.components.v1 as components
 
 # 1. Page Configuration
-st.set_page_config(page_title="IdeaTrix", layout="wide", page_icon="💡")
+st.set_page_config(page_title="IdeaTrixTRIAL", layout="wide", page_icon="💡")
 
 # Hide the default Streamlit button and adjust padding
 st.markdown(
